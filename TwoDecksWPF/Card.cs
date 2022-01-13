c182a329c1cc1e329eae68dab49f5fd251786269
Nathaniel Troncoso <ntroncoso05@gmail.com>
2022-01-13 18:47:29 -0400

    Add project files.

---
+++ TwoDecksWPF/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoDecksWPF
+{
+    class Card
+    {
+        public Values Value { get; private set; }
+        public Suits Suit { get; set; }
+
+        public Card(Values value, Suits suit)
+        {
+            Value = value;
+            Suit = suit;
+        }
+        public string Name { get { return $"{Value} of {Suit}"; } }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
